136daa26d282d4f967f3b060414b002f737f0008
Unkgod <28.danny.08@gmail.com>
2021-06-19 17:19:11 -0400

    images

--- src/web/routes.ts
+++ src/web/routes.ts
@@ -6,12 +6,12 @@ router.get("/", (req: Request, res: Response) => {
     '<body bgcolor="black">' +
       '<h1 style="margin: 80px 80px 0px 80px">' +
       '<font color="#ffffff">' +
-      "Kenny Fabricio Castro De La Quintana" +
+      "ll" +
       "</font>" +
       "</h1>" +
       '<p style="margin: 20px 0px 0px 80px">' +
       '<font color="#ffffff">' +
-      "Assignment 5, server running" +
+      "B, server running" +
       "</font>" +
       "</p>" +
       "</body>"
